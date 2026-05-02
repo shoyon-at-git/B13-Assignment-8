992711fbd9d2b860b03e99bea410bc4d0f2b2e9d
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-05-02 11:09:47 +0600

    Added products page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/my-products/page.js
@@ -0,0 +1,25 @@
+import ProductCard from "../components/ProductCard";
+
+export default async function ProductsPage() {
+
+  const res = await fetch("https://b13-assignment-8-five.vercel.app/data/product-data.json");
+  const products = await res.json();
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-10">
+
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-8">
+        All Summer Products ☀️
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+
+      </div>
+
+    </div>
+  );
+}
